d61ed2cd762cde09f16ea52511b9735f152ad816
Alexander Alemayhu <alexander@alemayhu.com>
2016-01-06 19:51:34 +0100

    enforce 80 column in c1d12a.swift

--- c1d12a.swift
+++ c1d12a.swift
@@ -1,42 +1,44 @@
 import Foundation
 
 func getIntegerLineOrZero() -> Int {
-  guard let line = readLine(stripNewline: true) else {
-    return 0
-  }
-
-  guard let integerValue = Int(line) else {
-    let trimmedLine = line.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
-      if let v = Int(trimmedLine) {
-        return v
-      }
-    return 0
-  }
-
-  return integerValue
+    guard let line = readLine(stripNewline: true) else {
+        return 0
+    }
+    
+    guard let integerValue = Int(line) else {
+        let characterSet = NSCharacterSet.whitespaceCharacterSet()
+        let trimmedLine =
+        line.stringByTrimmingCharactersInSet(characterSet)
+        if let v = Int(trimmedLine) {
+            return v
+        }
+        return 0
+    }
+    
+    return integerValue
 }
 
-  
-    func buildStaircase(topFloor: Int) -> [String] {
-        var stairs = [String]()
-        var index = 0
-        while index < topFloor {
-            var level = ""
-            var innerIndex = 0
-            repeat {
-                if innerIndex <= index {
-                    level += "#"
-                } else {
-                    level += " "
-                }
-                innerIndex++
-            } while innerIndex < topFloor
-            stairs.append(level)
-            index++
-        }
-        
-        return stairs.map { String($0.characters.reverse()) }
+
+func buildStaircase(topFloor: Int) -> [String] {
+    var stairs = [String]()
+    var index = 0
+    while index < topFloor {
+        var level = ""
+        var innerIndex = 0
+        repeat {
+            if innerIndex <= index {
+                level += "#"
+            } else {
+                level += " "
+            }
+            innerIndex++
+        } while innerIndex < topFloor
+        stairs.append(level)
+        index++
     }
+    
+    return stairs.map { String($0.characters.reverse()) }
+}
 
 
 var tries = getIntegerLineOrZero()
